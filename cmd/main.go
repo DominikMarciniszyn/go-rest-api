a37d5db3826ead36c104734979a57339c6f619be
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2022-06-23 19:52:37 +0200

    refactor: moved all server stuff into separate package

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,9 @@
 package main
 
 import (
-	"go-rest-api/internal/database/config"
-	"go-rest-api/internal/routes"
-	"log"
-
-	"github.com/gofiber/fiber"
+	"go-rest-api/cmd/server"
 )
 
 func main() {
-	app := fiber.New()
-
-	api := app.Group("/api")
-	config.Connect()
-
-	v1 := api.Group("/v1")
-	v1.Get("/ping", routes.Ping)
-	v1.Get("/orders", routes.GetOrders)
-	v1.Get("/orders/:id", routes.GetOrder)
-	v1.Post("/orders", routes.CreateOrder)
-	v1.Put("/orders/:id", routes.UpdateOrder)
-	v1.Delete("/orders/:id", routes.RemoveOrder)
-
-	log.Fatal(app.Listen(":3000"))
+	server.StartServer()
 }
