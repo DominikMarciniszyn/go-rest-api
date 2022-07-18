2e7384dc1268be5d07f90686234eeaa3c284e2fa
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2022-07-18 19:52:51 +0200

    refactor: simplified main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,43 +1,16 @@
 package main
 
 import (
-	"github.com/gofiber/fiber"
 	"github.com/rs/zerolog/log"
 	"go-rest-api/internal"
-	"go-rest-api/internal/config"
 	"os"
-	"os/signal"
 )
 
 func main() {
-	config.InitServiceConfig()
-
-	if err := internal.Invoke(startHttpServer); err != nil {
-		panic(err)
+	if err := internal.Run(); err != nil {
+		log.Err(err).Msg("Something went terribly wrong!")
+		os.Exit(1)
 	}
-}
 
-func startHttpServer(server *fiber.App) error {
-	done := make(chan bool, 1)
-	quit := make(chan os.Signal, 1)
-
-	signal.Notify(quit, os.Interrupt)
-
-	go func() {
-		<-quit
-		log.Trace().Msg("Received SIGINT")
-
-		err := server.Shutdown()
-
-		if err != nil {
-			return
-		}
-
-		log.Trace().Msg("Shutdown gracefully")
-
-		done <- true
-	}()
-
-	<-done
-	return nil
+	os.Exit(0)
 }
