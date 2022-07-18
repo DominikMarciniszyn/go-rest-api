dc56dc75e640069e89e49a844226929c784500b9
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2022-07-18 19:52:04 +0200

    feat: added logger

---
+++ internal/logger.go
@@ -0,0 +1,33 @@
+package internal
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"go-rest-api/internal/config"
+	"os"
+	"time"
+)
+
+func provideLogger(config *config.Config) *zerolog.Logger {
+	logLevel, err := zerolog.ParseLevel(config.LogLevel)
+
+	if err != nil {
+		panic(err)
+	}
+
+	zerolog.SetGlobalLevel(logLevel)
+	zerolog.TimeFieldFormat = time.RFC3339
+
+	if config.Pretty {
+		log.Logger = log.Output(zerolog.ConsoleWriter{
+			Out:        os.Stdout,
+			TimeFormat: "15:04:05",
+		})
+
+		log.Logger = log.Logger.With().Caller().Logger()
+	}
+
+	log.Logger = log.Logger.With().Str("version", "alpha").Logger()
+
+	return &log.Logger
+}
